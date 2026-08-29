561f1abf8dfff2b925faff62c723d76eb7694532
AnUbH75 <anubhavas75@gmail.com>
2026-08-30 02:09:34 +0530

    Adding market_data to main

--- src/wealthdock_server/main.py
+++ src/wealthdock_server/main.py
@@ -7,6 +7,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
+from wealthdock_server.api.v1 import market_data
 from wealthdock_server.api.v1.auth import router as auth_router
 from wealthdock_server.api.v1.sync import router as sync_router
 from wealthdock_server.core.config import get_settings
